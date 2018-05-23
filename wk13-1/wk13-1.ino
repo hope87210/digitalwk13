4b0567a91a9c6ebbde84dd55547437fa80edd1bb
hope87210 <39110439+hope87210@users.noreply.github.com>
2018-05-23 11:46:57 +0800

    Add files via upload

---
+++ wk13-1/wk13-1.ino
@@ -0,0 +1,35 @@
+const int ledCount = 10;    // the number of LEDs in the bar graph
+
+int ledPins[] = { 
+  2, 3, 4, 5, 6, 7,8,9,10,11 };   // an array of pin numbers to which LEDs are attached
+
+
+void setup() {
+  // loop over the pin array and set them all to output:
+  for (int thisLed = 0; thisLed < ledCount; thisLed++) {
+    pinMode(ledPins[thisLed], OUTPUT); 
+  }
+}
+
+void loop() {
+
+
+  // loop over the LED array:
+  for (int thisLed = 0; thisLed < ledCount; thisLed++) {
+    digitalWrite(ledPins[thisLed],HIGH);
+    delay(60);
+    digitalWrite(ledPins[thisLed],LOW);
+    } 
+ for (int thisLed = ledCount -1;thisLed>=0;thisLed--){
+   digitalWrite(ledPins[thisLed],HIGH);
+   delay(60);
+   if(thisLed ==0){
+   delay(200);}
+   else{
+   digitalWrite(ledPins[thisLed],LOW);  
+   }
+ }
+   
+ 
+}
+
---
+++ wk13-2/wk13-2.ino
@@ -0,0 +1,27 @@
+const int ledCount = 10;    // the number of LEDs in the bar graph
+
+int ledPins[] = { 
+  2, 3, 4, 5, 6, 7,8,9,10,11 };   // an array of pin numbers to which LEDs are attached
+
+
+void setup() {
+  // loop over the pin array and set them all to output:
+  for (int thisLed = 0; thisLed < ledCount; thisLed++) {
+    pinMode(ledPins[thisLed], OUTPUT); 
+  }
+}
+
+void loop() {
+
+
+  // loop over the LED array:
+  for (int thisLed = 0; thisLed < ledCount; thisLed++) {
+    digitalWrite(ledPins[thisLed],HIGH);
+    digitalWrite(ledPins[9-thisLed],HIGH);
+    delay(60);
+    digitalWrite(ledPins[thisLed],LOW);
+    digitalWrite(ledPins[9-thisLed],LOW);
+    } 
+
+}
+
